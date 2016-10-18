88edccf64f160f42c825d3db3476d302ad021ffd
Maycol <Maycol@MAYCOL_MASOFT>
2016-10-18 13:04:25 -0500

    dfgdfg

---
+++ FrameworkMVC/view/css/Contable/js/procesos-contable.js
@@ -0,0 +1,23 @@
+
+		$(document).ready(function(){
+			load_contable(1);
+		});
+		
+		
+		function load_contable(page){
+			var c= $("#c").val();
+			$("#contable").fadeIn('slow');
+			$.ajax({
+				url:'view/css/Contable/ajax/cargar_contable.php?action=ajax&page='+page+'&c='+c,
+				 beforeSend: function(objeto){
+				$("#contable").html('<img src="view/images/ajax-loader.gif"> Cargando...');
+				},
+				success:function(data){
+					$(".div_contable").html(data).fadeIn('slow');
+					$("#contable").html("");
+				}
+			})
+		}
+        
+		
+		
